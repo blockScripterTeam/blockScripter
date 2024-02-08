9818dfbc6f150495d367172d82f4f2ca36812bec
zszfympx <2792668017@qq.com>
2024-02-08 17:48:29 +0800

    try something rust⚡⚡⚡

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+include!("utils/forge_utils.rs");
+
 fn main() {
     tauri::Builder::default()
         .run(tauri::generate_context!())
