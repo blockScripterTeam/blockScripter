ec6b640ef97733c97843605325dff424ef8e9b45
刘lyxAndy <liuyuxuanandy@outlook.com>
2024-02-10 22:50:44 +0800

    feat: ajax实现toolbox分文件

--- src/components/injectBlockly.vue
+++ src/components/injectBlockly.vue
@@ -2,33 +2,17 @@
 import Blockly from 'blockly'
 import * as Ch from 'blockly/msg/zh-hans'
 import {javascriptGenerator} from 'blockly/javascript';
+import axios from 'axios';
+
 Blockly.setLocale(Ch)
+
+const toolboxXml = await axios.get('/assets/toolbox.xml')
+
 export default {
     data() {
         return {
             workspace: null,
-            toolboxXml:
-                `
-<xml id="toolbox" style="display: none">
-    <category name="逻辑" categorystyle="logic_category">
-        <block type="controls_if"></block>
-        <block type="logic_compare"></block>
-        <block type="logic_operation"></block>
-        <block type="logic_negate"></block>
-        <block type="logic_boolean"></block>
-    </category>
-    <category name="循环" categorystyle="loop_category">
-        <block type="controls_repeat_ext">
-            <value name="TIMES">
-                <shadow type="math_number">
-                    <field name="NUM">10</field>
-                </shadow>
-            </value>
-        </block>
-        <block type="controls_flow_statements"></block>
-    </category>
-</xml>
-            `
+            toolboxXml: toolboxXml.data
         }
     },
     mounted() {
