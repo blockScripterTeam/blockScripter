eecce3e5c1fd5312e4f1bcf71f6514479c671b2a
Tigercrl <tigercrl@icloud.com>
2024-02-11 21:33:41 +0800

    feat: Build actions in dev branch

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import {createRouter, createWebHistory} from "vue-router";
-import MainMenuView from "../views/MainMenuView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
