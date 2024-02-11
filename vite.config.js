6f136e14bca773abb25c2d0f8eaa2867ca1bdde6
刘lyxAndy <liuyuxuanandy@outlook.com>
2024-02-11 22:01:13 +0800

    feat: add something rust&tauri

--- vite.config.js
+++ vite.config.js
@@ -66,5 +66,18 @@ export default defineConfig({
       threshold: 1024 * 1024
     }),
     visualizer()
-  ]
+  ],
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 })
