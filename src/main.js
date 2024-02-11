e0b49c8427dee2e0adddd1bca242a8ee0846ca69
Tigercrl <tigercrl@icloud.com>
2024-02-11 21:25:58 +0800

    revert: Deleted some codes that should be in the website

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import './styles.css'
 import router from "./router/index.js";
-
 import { createApp } from "vue";
-import App from "./views/MainMenuView.vue";
 
 const app = createApp(App);
 app.use(router);
--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,7 @@ import MainMenuView from "../views/MainMenuView.vue";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        {
-            path: "/",
-            name: "main-menu",
-            component: MainMenuView,
-        }
+
     ],
 });
 
